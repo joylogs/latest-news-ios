aa627208a53f4ee0cb21e43c13842759af54e05e
Joy <joy.logs@gmail.com>
2024-01-02 21:30:57 +0530

    initial commit project

---
+++ LatestNews/LatestNews/FeedView.swift
@@ -0,0 +1,25 @@
+//
+//  FeedView.swift
+//  LatestNews
+//
+//  Created by Joy Banerjee on 02/01/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct FeedView: View {
+    
+    var body: some View {
+        VStack {
+            Label("NewsLabel", image: "")
+        }        
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+
+    static var previews: some View {
+        FeedView()
+    }
+}
---
+++ LatestNews/LatestNews/LatestNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LatestNewsApp.swift
+//  LatestNews
+//
+//  Created by Joy Banerjee on 02/01/24.
+//
+
+import SwiftUI
+
+@main
+struct LatestNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
