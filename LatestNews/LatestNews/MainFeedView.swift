4286043982b4fc6c7de173b2734dab99b468790a
Joy <joy.logs@gmail.com>
2024-01-06 00:03:40 +0530

    code cleanup

--- LatestNews/LatestNews/MainFeedView.swift
+++ LatestNews/LatestNews/MainFeedView.swift
@@ -12,48 +12,11 @@ struct MainFeedView: View {
     let feeds = FeedModel.feedData()
     
     var body: some View {
-//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
         List(feeds) { feed in
             Text(feed.description)
             
-//            Group {
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//            }
-//
-//            Group {
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//            }
-//
-//            Group {
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//                FeedView()
-//            }
+
         }
     }
 }
